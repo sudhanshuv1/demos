51b996c956b9d690d62a41510eb1aef5cf894ba2
Sudhanshu Tiwari <sudhanshu.98t@gmail.com>
2025-03-02 00:57:52 +0530

    src: Improve formatting for main.py in Shortcuts Window

--- src/Shortcuts Window/main.py
+++ src/Shortcuts Window/main.py
@@ -4,5 +4,3 @@ shortcuts_window = workbench.builder.get_object("shortcuts_window")
 button = workbench.builder.get_object("button")
 
 button.connect("clicked", lambda *_: shortcuts_window.present())
-
-
